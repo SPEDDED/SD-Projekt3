5b649b20e3b0e62e9a2164dcaacb5378de19ee83
284143-szef <284143@student.pwr.edu.pl>
2026-06-07 15:17:39 +0200

    Add files via upload

---
+++ include/HashTableAVL.h
@@ -0,0 +1,49 @@
+#ifndef HASHTABLEAVL_H
+#define HASHTABLEAVL_H
+
+#include "HashTable.h"
+
+class HashTableAVL : public HashTable { // Dziedziczenie z HashTable
+private:
+
+    struct AVLNode {
+        int key;
+        int value;
+        int height; //wysokość węzła w drzewie
+
+
+        AVLNode* left; //wskażnik na lawe dziecko
+        AVLNode* right;//wskażnik na prawe dziecko
+
+        AVLNode(int k, int v) : key(k), value(v), height(1), left(nullptr), right(nullptr) {} //konstruktor węzła
+    };
+
+    AVLNode** table;
+
+    int height(AVLNode* node);
+    int getBalance(AVLNode* node);
+
+
+    AVLNode* rightRotate(AVLNode* y);
+    AVLNode* leftRotate(AVLNode* x);
+    AVLNode* insertNode(AVLNode* node, int key, int value, bool& success);
+    AVLNode* minValueNode(AVLNode* node);
+    AVLNode* removeNode(AVLNode* root, int key, bool& success);
+
+
+    int* findNode(AVLNode* node, int key);
+    void displayTree(AVLNode* node);
+    void clearTree(AVLNode* node);
+
+public:
+    HashTableAVL(int cap); //konstruktor
+    ~HashTableAVL() override;//dekonstruktor
+
+    bool insert(int key, int value) override;
+    bool remove(int key) override;
+    int* find(int key) override;
+    void display() override;
+    void clear() override;
+};
+
+#endif
